b92125ba4fd05282ecd18296d2fcc6c715e62a89
Stanislav Lysunenko <sidux77@gmail.com>
2022-05-07 14:02:04 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InformationSearcher searcher = new AirportInformationSearcher("flights.json", "Cargos.json");
+
+        //Date date1 = new Date();
+        //System.out.println(searcher.getAirportInformation("SVE", date1));
+    }
+}
