b9ab3ca46ce8ecffbcf58e09a93b14ec877ed638
Stanislav Lysunenko <sidux77@gmail.com>
2022-05-08 16:45:25 +0200

    Added Utility Classes for Weight and Date

--- src/main/java/DateUtil.java
+++ src/main/java/DateUtil.java
@@ -2,8 +2,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+// Utility class for formatting Dates
 public class DateUtil {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ssz", Locale.ENGLISH);
+    // Formatter for all Date objects
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ssz", Locale.ENGLISH);
+
+    // Method to parse Strings to a LocalDate objects
     public static LocalDate parseDate(String date_str) {
         return LocalDate.parse(date_str, formatter);
     }
--- src/main/java/WeightUtil.java
+++ src/main/java/WeightUtil.java
@@ -1,6 +1,9 @@
+// Utility class for Weight objects
 public class WeightUtil {
+    // Method to convert kilograms to pounds
     public static double kgTolb(long kg) { return kg * 2.2046;}
 
+    // Method to convert pounds to kilograms
     public static double lbTokg(long lb) {
         return lb / 2.205;
     }
